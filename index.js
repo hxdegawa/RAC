c81b6d4f442b1c32f53fccb921ccdcce30c90ee6
出川　大和 <hxdegawa@gmail.com>
2017-09-29 00:34:57 +0900

    Add unfinished video list

--- index.js
+++ index.js
@@ -2,63 +2,97 @@ $(function(){
   
   let control = {
     fastForward: false,
-    fastRewind: false,
-    muted: false
-  };
+     fastRewind: false,
+          muted: false
+  },
+      unFinishedTitle = [],
+      unFinishedURL = [];
   
-  $("head").prepend('<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />');
-  $("body").append('<div id="movie-controller"></div>');
-  $("#movie-controller").append('<div class="swich-controller swich-left"><i class="material-icons">chevron_left</i></div><div class="swich-controller master"><i class="material-icons">settings</i></div><div class="swich-controller swich-right"><i class="material-icons">chevron_right</i></div><br /><div class="swich-controller col5 rate-left"><i class="material-icons">fast_rewind</i></div><div class="swich-controller col5 mute"><i class="material-icons">volume_mute</i></div><div class="swich-controller col5 fullscreen"><i class="material-icons">fullscreen</i></div><a class="movie-download-link" target="_blank"><div class="swich-controller col5 download"><i class="material-icons">file_download</i></div></a><div class="swich-controller col5 rate-right"><i class="material-icons">fast_forward</i></div>');
-  
-  $(".movie-download-link").attr({"title": "Download", "download": "Classroom.mp4", "href": document.getElementsByTagName("source")[0].src});
-  
-  $(".swich-right").click(function(){
-    document.getElementsByTagName("video")[0].currentTime += 10;
-  });
-  
-  $(".swich-left").click(function(){
-    document.getElementsByTagName("video")[0].currentTime -= 10;
-  });
-  
-  $(".rate-right").click(function(){
-    control.fastForward = !control.fastForward;
-    control.fastRewind = false;
-    if(control.fastForward){
-      document.getElementsByTagName("video")[0].playbackRate = 2;
-    }else{
-      document.getElementsByTagName("video")[0].playbackRate = 1;
-    }
-  });
-  
-  $(".rate-left").click(function(){
-    control.fastRewind = !control.fastRewind;
-    control.fastForward = false;
-    if(control.fastRewind){
-      document.getElementsByTagName("video")[0].playbackRate = 0.5;
-    }else{
-      document.getElementsByTagName("video")[0].playbackRate = 1;
-    }
-  });
-  
-  $(".master").click(function(){
-    $("#movie-controller").toggleClass("advanced");
-  });
-  
-  $(".mute").click(function(){
-    control.muted = !control.muted;
-    if(control.muted){
-      document.getElementsByTagName("video")[0].muted = true;
-    }else{
-      document.getElementsByTagName("video")[0].muted = false;
-    }
-  });
-  
-  $(".fullscreen").click(function(){
-    document.getElementsByTagName("video")[0].webkitRequestFullscreen();
-  });
-   
-  $("#video01").on("timeupdate", function(){
+  if(location.href.indexOf('https://ww3.tokyo-shoseki.co.jp/api/dwango/requestContents.php?') != -1){
+
+    $("body").append('<iframe id="unfinished-list" style="display: none; margin: 0px; padding: 0px;" src="https://secure.nnn.ed.jp/mypage/report/pc/list/index"></iframe>');
+    $("head").prepend('<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />');
+    $("body").append('<div id="movie-controller"></div>');
+    $("#movie-controller").append('<div class="swich-controller swich-left"><i class="material-icons">chevron_left</i></div><div class="swich-controller master"><i class="material-icons">settings</i></div><div class="swich-controller swich-right"><i class="material-icons">chevron_right</i></div><br /><div class="swich-controller col5 rate-left"><i class="material-icons">fast_rewind</i></div><div class="swich-controller col5 mute"><i class="material-icons">volume_mute</i></div><div class="swich-controller col5 fullscreen"><i class="material-icons">fullscreen</i></div><div class="swich-controller col5 check-list"><i class="material-icons">list</i></div><a class="movie-download-link" target="_blank"><div class="swich-controller col5 download"><i class="material-icons">file_download</i></div></a><div class="swich-controller col5 rate-right"><i class="material-icons">fast_forward</i></div>');
+
+    $(".movie-download-link").attr({"title": "Download", "download": "Classroom.mp4", "href": document.getElementsByTagName("source")[0].src});
+
+    $(".swich-right").click(function(){
+      document.getElementsByTagName("video")[0].currentTime += 10;
+    });
+
+    $(".swich-left").click(function(){
+      document.getElementsByTagName("video")[0].currentTime -= 10;
+    });
+
+    $(".rate-right").click(function(){
+      control.fastForward = !control.fastForward;
+      control.fastRewind = false;
+      if(control.fastForward){
+        document.getElementsByTagName("video")[0].playbackRate = 2;
+      }else{
+        document.getElementsByTagName("video")[0].playbackRate = 1;
+      }
+    });
+
+    $(".rate-left").click(function(){
+      control.fastRewind = !control.fastRewind;
+      control.fastForward = false;
+      if(control.fastRewind){
+        document.getElementsByTagName("video")[0].playbackRate = 0.5;
+      }else{
+        document.getElementsByTagName("video")[0].playbackRate = 1;
+      }
+    });
+
+    $(".master").click(function(){
+      $("#movie-controller").toggleClass("advanced");
+    });
+
+    $(".mute").click(function(){
+      control.muted = !control.muted;
+      if(control.muted){
+        document.getElementsByTagName("video")[0].muted = true;
+      }else{
+        document.getElementsByTagName("video")[0].muted = false;
+      }
+    });
+
+    $(".fullscreen").click(function(){
+      document.getElementsByTagName("video")[0].webkitRequestFullscreen();
+    });
     
-  });
+    $(".check-list").click(function(){
+      $(".undone-list-container").toggleClass("visible");
+    });
+
+    $("#video01").on("timeupdate", function(){
+
+    });
+
   
+  
+//  create list of undone report
+  
+    $(function(){
+
+      $("body").append('<div class="undone-list-container"><h1>未完了レポート</h1></div>');
+      
+      $.ajax({
+        type: 'GET',
+        url: "https://secure.nnn.ed.jp/mypage/report/pc/list/index",
+        dataType: 'html',
+        success: function(data) {
+          var result = $($.parseHTML(data));
+          for(var i = 0; i < result.eq(9).find(".normal > a").length; i++){unFinishedTitle.push(result.eq(9).find(".normal > a")[i].text)};
+          for(var i = 0; i < result.eq(9).find(".normal > a").length; i++){unFinishedURL.push(result.eq(9).find(".normal > a")[i].href)};
+          for(var i = 0; i < result.eq(9).find(".normal > a").length; i++){
+            $(".undone-list-container").append('<p class="undone-list"><a target="_blank" href="' + unFinishedURL[i] + '">' + unFinishedTitle[i] + '</a></p><hr />');
+          };
+        }, error:function(e) {
+          console.log(e);
+        }
+      });
+    });
+  };
 });
