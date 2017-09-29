05c7f1193c52e2da4dc48685ffcdde8df9eef64b
出川　大和 <hxdegawa@gmail.com>
2017-09-30 01:51:51 +0900

    [COMPLETE] Added volume-control function & progress-bar function

--- index.js
+++ index.js
@@ -16,14 +16,21 @@ $(function(){
      fastRewind: false,
           muted: false
   },
+        volumeStatus = [
+          {"status": "最大", "volume": 1, "icon": "volume_up"},
+          {"status": "中間", "volume": 0.5, "icon": "volume_down"},
+          {"status": "最小", "volume": 0.2, "icon": "volume_mute"}
+        ],
         unFinishedTitle = [],
         unFinishedURL = [],
-        toastShow; 
+        volumeIndex = 1,
+        toastShow,
+        exhibition;
 
     $("body").append('<div class="movie-toast"><span></span></div><div id="movie-controller"></div><div class="undone-list-container"><h1>未完了レポート</h1><div class="undone-list-container-close"><i class="material-icons">close</i></div></div>');
-    $("body").prepend('<div class="controller"><div class="controller-inner-container"><div class="progress-bar-container"><div class="progress-bar"></div></div><p><span class="movie-time"></span><span>/</span><span class="movie-duration"></span></p></div></div>');
+    $("body").prepend('<div class="controller"><div class="controller-inner-container"><div class="progress-bar-container"><div class="progress-bar"></div></div><p><span class="movie-time"></span><span> / </span><span class="movie-duration"></span></p></div></div>');
     $("head").prepend('<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />');
-    $("#movie-controller").append('<div class="swich-controller swich-left"><i class="material-icons">chevron_left</i></div><div class="swich-controller control"><i class="material-icons">videogame_asset</i></div><div class="swich-controller master"><i class="material-icons">settings</i></div><div class="swich-controller volume"><i class="material-icons">volume_up</i></div><div class="swich-controller swich-right"><i class="material-icons">chevron_right</i></div><br /><div class="swich-controller col5 rate-left"><i class="material-icons">fast_rewind</i></div><div class="swich-controller col5 mute"><i class="material-icons">volume_off</i></div><div class="swich-controller col5 fullscreen"><i class="material-icons">fullscreen</i></div><div class="swich-controller col5 check-list"><i class="material-icons">list</i></div><a class="movie-download-link" target="_blank"><div class="swich-controller col5 download"><i class="material-icons">file_download</i></div></a><div class="swich-controller col5 rate-right"><i class="material-icons">fast_forward</i></div>');
+    $("#movie-controller").append('<div class="swich-controller swich-left"><i class="material-icons">chevron_left</i></div><div class="swich-controller control"><i class="material-icons">videogame_asset</i></div><div class="swich-controller master"><i class="material-icons">settings</i></div><div class="swich-controller volume"><i class="material-icons volume-icon">volume_up</i></div><div class="swich-controller swich-right"><i class="material-icons">chevron_right</i></div><br /><div class="swich-controller col5 rate-left"><i class="material-icons">fast_rewind</i></div><div class="swich-controller col5 mute"><i class="material-icons">volume_off</i></div><div class="swich-controller col5 fullscreen"><i class="material-icons">fullscreen</i></div><div class="swich-controller col5 check-list"><i class="material-icons">list</i></div><a class="movie-download-link" target="_blank"><div class="swich-controller col5 download"><i class="material-icons">file_download</i></div></a><div class="swich-controller col5 rate-right"><i class="material-icons">fast_forward</i></div>');
 
     //  create list of undone report
   
@@ -45,7 +52,7 @@ $(function(){
         }
       });
       
-      setTimeout(function(){$("#video01").removeAttr("controls"); $(".movie-duration").text(document.getElementsByTagName("video")[0].duration.toFixed(0))}, 1000);
+      setTimeout(function(){$("#video01").removeAttr("controls"); $(".movie-duration").text((Math.floor(document.getElementsByTagName("video")[0].duration / 60)) + ":" + ("0" + Math.round(document.getElementsByTagName("video")[0].duration % 60)).slice(-2))}, 1000);
       
     });
 
@@ -53,10 +60,23 @@ $(function(){
 
     $(".control").click(function(){
       $(".controller").toggleClass("visible");
+      if($(".controller").hasClass("visible")){
+        $(".controller").addClass("first-exhibition");
+        window.clearTimeout(exhibition);
+        exhibition = setTimeout(function(){$(".controller").removeClass("first-exhibition")},1500);
+      }
     });
     
     $(".volume").click(function(){
-      
+        document.getElementsByTagName("video")[0].volume = volumeStatus[volumeIndex].volume;
+        toast("音量を" + volumeStatus[volumeIndex].status + "に設定");
+        $(".volume-icon").text(volumeStatus[volumeIndex].icon);
+      console.log(volumeIndex);
+      if(volumeIndex !== 2){
+        volumeIndex ++;
+      }else{
+        volumeIndex = 0;
+      };
     });
     
     $(".swich-right").click(function(){
@@ -115,14 +135,21 @@ $(function(){
       toast("未完了リストを表示");
     });
 
-    $("#video01").on("timeupdate", function(){
-      $(".progress-bar").css("width", this.currentTime / this.duration * 100 + "%");
-      $(".movie-time").text(this.currentTime.toFixed(0));
+    $(".undone-list-container-close").click(function(){
+      $(".undone-list-container").removeClass("visible");
     });
     
-    $(".undone-list-container-close").click(function(){
-        $(".undone-list-container").removeClass("visible");
-      });
+    $(".progress-bar-container").click(function(e){
+      $(".progress-bar").css("width", e.offsetX / 585 * 100 + "%");
+      document.getElementsByTagName("video")[0].currentTime = document.getElementsByTagName("video")[0].duration * e.offsetX / 585;
+      document.getElementsByTagName("video")[0].play();
+    });
+    
+    $("#video01").on("timeupdate", function(){
+      $(".progress-bar").css("width", this.currentTime / this.duration * 100 + "%");
+      $(".movie-time").text(Math.floor(this.currentTime / 60) + ":" + ("0" + Math.round(this.currentTime % 60)).slice(-2));
+    });
+    
     
     function toast(message){
       $(".movie-toast > span").text(message);
