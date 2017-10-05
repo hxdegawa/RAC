5976951f9bf139fde229672f4cbdba9e1b8f98b9
出川　大和 <hxdegawa@gmail.com>
2017-10-06 02:32:46 +0900

    Added function card descs & Installation guide in option page

--- option/options.js
+++ option/options.js
@@ -1,5 +1,38 @@
 $(function(){
+  
+  $(window).scroll(function(){
+    if($(this).scrollTop() >= 800){
+      $(".landing-view").addClass("hide");
+    };
+  });
+  
   $(".btn-start").click(function(){
     $("html,body").animate({ scrollTop: $(".desc-container").offset().top - 64});
   });
+  
+  $('li[class*="tab-"]').click(function(){
+    resetAllWindow();
+  });
+  
+  $(".tab-function").click(function(){
+    $(".container-function").addClass("active");
+  });
+  
+  $(".tab-installation").click(function(){
+    $(".container-installation").addClass("active");
+    $(".desc-container").addClass("active");
+  });
+  
+  $(".tab-contact").click(function(){
+//    $(".container-contact").addClass("active");
+  });
+  
+  $(".tab-donation").click(function(){
+//    $(".container-donation").addClass("active");
+  });
+  
+  function resetAllWindow(){
+    $('div[class*="container-"], .desc-container').removeClass("active");
+  };
+  
 });
