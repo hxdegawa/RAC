6c75b411135eb3bf8d1d5f7a31ef03ec528493de
Motone Adachi <motone_16n1100030@nnn.ed.jp>
2017-10-06 20:47:47 +0900

    fixed for Contact.*Sending Messege was achieved.

--- option/options.js
+++ option/options.js
@@ -1,20 +1,20 @@
 $(function(){
-  
+
   $(window).on("resize", function(){
     checkWidth();
   });
-  
+
   $(window).scroll(function(){
     if($(this).scrollTop() >= $(window).height()){
       $(".landing-view").addClass("hide");
       $(".desc-container").removeClass("disabled");
     };
   });
-  
+
   $(".btn-start").click(function(){
     $("html,body").animate({scrollTop: $(".desc-container").offset().top - 64});
   });
-  
+
   $(".logo-image").click(function(){
     $(".landing-view").removeClass("hide");
     console.log($(window).height());
@@ -22,32 +22,32 @@ $(function(){
     $("html,body").animate({scrollTop: $("body").offset().top});
     $(".desc-container").addClass("disabled");
   });
-  
+
   $('li[class*="tab-"]').click(function(){
     resetAllWindow();
   });
-  
+
   $(".tab-function").click(function(){
     $(".container-function").addClass("active");
   });
-  
+
   $(".tab-installation").click(function(){
     $(".container-installation").addClass("active");
     $(".desc-container").addClass("active");
   });
-  
+
   $(".tab-contact").click(function(){
     $(".container-contact").addClass("active");
   });
-  
+
   $(".tab-donation").click(function(){
     $(".container-donation").addClass("active");
   });
-  
+
   $(".mail-form-submit").click(function(){
     mailSubmit();
   });
-  
+
   function checkWidth(){
     if($(window).width() < 800){
       $(".following-step-1 p").eq(0).text("Prepare your computer.");
@@ -57,18 +57,18 @@ $(function(){
       $(".following-step-5 p").eq(0).text("Finish!");
     }
   }
-  
+
   function resetAllWindow(){
     $('div[class*="container-"], .desc-container').removeClass("active");
   };
-  
+
   function mailSubmit(){
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://maker.ifttt.com/trigger/mail_submit/with/key/chZq40Tri-7F73obbMC_IK", false);
+    xhr.open("GET", "https://maker.ifttt.com/trigger/mail_submit/with/key/chZq40Tri-7F73obbMC_IK?value1="+$(".name").val()+"&value2="+$(".mail").val()+"&value3="+$(".opinion").val());
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify({value1: $(".name")[0].value, value2: $(".mail")[0].value, value3: $(".opinion")[0].value}));
+    xhr.send();
   }
-  
+
   checkWidth();
-  
+
 });
